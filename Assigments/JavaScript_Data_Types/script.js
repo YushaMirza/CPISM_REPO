3cb0e1dfa8b6168630e720ef283c8e83d99dbe60
YushaMirza <yousha.mirza328@gmail.com>
2024-11-28 22:57:05 +0500

    CPISM

---
+++ Assigments/JavaScript_Data_Types/script.js
@@ -0,0 +1,6 @@
+let my_Name = "Yusha Mirza";
+let my_Father_Name = "Wajahat Mirza";
+let age = 17;
+let qualification = "Matric";
+
+document.write(`<h1 style= 'color: blue' > My Name is &nbsp ${my_Name} <br> <br> My Father Name is ${my_Father_Name} </h1> <h2 style= 'color: purple'> <br> My Age is ${age} <br><br> My Qualification is ${qualification} </h2> `);
